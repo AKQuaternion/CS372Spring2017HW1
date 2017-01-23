3f2db89e1e3a9856ed9d5420ffe910a27c813095
Chris Hartman <chris.m.hartman@gmail.com>
2017-01-23 14:31:59 -0900

    Initial commit

---
+++ CS372Spring2017HW1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS372Spring2017HW1
+//
+//  Created by Chris Hartman on 1/23/17.
+//  Copyright © 2017 Chris Hartman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
