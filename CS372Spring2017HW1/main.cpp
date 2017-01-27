a2fa74f97649fbb8ec31ec5a39d0ef8da64bbf1e
Dope Beats <reweeden@alaska.edu>
2017-01-27 12:27:10 -0900

    Added Rohan's grade

--- CS372Spring2017HW1/main.cpp
+++ CS372Spring2017HW1/main.cpp
@@ -14,5 +14,18 @@ int main(int argc, const char * argv[]) {
     cout << "Professor Hartman does not need a grade." << endl;
     cout << "Alexander Eckert should get an A on Assignment 1,\n";
     cout << "because he was the first student doing it." << endl;
+	char x = 82;
+	cout << x << (char) (x + 29) << (char)((x & 205) | 40);
+	x += 271;
+	cout << x << (char)((x - 42) << 1) << (char)((x >> 2) + 15);
+	int y[] = {230, 64, 142, 228, 194, 200};
+	char c = 0;
+	for(int xD = 364; (xD % 3) == 1; xD /= 3) {
+		cout << (char)(y[c] >> 1);
+		c++;
+	}
+	cout << (char)(x + 4) << (char)(123 & 186) << (char)(4 << 3) << (char) 65;
+	
+	cout << endl;
     return 0;
 }
