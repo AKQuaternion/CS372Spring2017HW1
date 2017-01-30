c753d7bbfc5691fe69b49ebae841a48c2dcbe8f1
Jarye Murphy <jmurphy11@alaska.edu>
2017-01-29 19:11:27 -0900

    added in my name and grade into main.cpp

--- CS372Spring2017HW1/main.cpp
+++ CS372Spring2017HW1/main.cpp
@@ -35,6 +35,8 @@ int main(int argc, const char * argv[]) {
 	cout << "Jeremiah Jacobson should get an A on assignment 1" << endl;
 	
 	cout << "Aisha Peters should get an A on Assignment 1." << endl;
+
+	cout << "Jarye, A grade on hw 1 " << endl;
 	
     return 0;
 }
